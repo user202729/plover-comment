eba3bafbf117094665556cb94bf99cdc0fb670c9
user202729 <25191436+user202729@users.noreply.github.com>
2022-01-10 18:00:25 +0700

    Make type annotation future

--- plover_comment/__init__.py
+++ plover_comment/__init__.py
@@ -4,5 +4,5 @@ import time
 def comment(engine: 'plover.engine.StenoEngine', argument: str)->None:
 	pass
 
-def comment_meta(context: 'plover.formatting._Context', argument: str) -> plover.formatting._Action:
+def comment_meta(context: 'plover.formatting._Context', argument: str) -> 'plover.formatting._Action':
 	return context.copy_last_action()
